f6c8ac9fc84b1c07f99be09d96204b26e3fcec9e
Aries0331 <jinzhu@ualberta.ca>
2017-04-13 10:45:56 -0600

    init commit, can follow command now

---
+++ Assets/Scripts/_CreateObject.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public partial class Wit3D : MonoBehaviour {
+
+	void CreateObjectHandler(string textToParse){
+
+		// Debug: print
+		print ("CREATE OBJECT function called");
+
+		// Parse JSON
+		var N = JSON.Parse (textToParse);
+
+		// Find the entities: subject, destination, and new_name
+//		string subjJson = N["outcomes"][0]["entities"]["subject"][0]["value"].Value.ToLower();
+//		string newNameJson = N["outcomes"][0]["entities"]["new_name"][0]["value"].Value.ToLower();
+//		string destJson = N["outcomes"][0]["entities"]["destination"][0]["value"].Value.ToLower();
+//		int qtyJson = N ["outcomes"] [0] ["entities"] ["number"] [0] ["value"].AsInt;
+
+		// Find the entities: Corgi the dog, action
+		//string dog =  N["outcomes"][0]["entities"]["intent"][0]["value"].Value.ToLower();
+		string action =  N["outcomes"][0]["entities"]["action"][0]["value"].Value.ToLower();
+		print (N ["outcomes"] [0] ["entities"]);
+		print ("========" + action);
+
+		// State machine:
+
+		// All should have at least a subject
+		GameObject subject = GameObject.Find("Corgi");
+
+//		if (qtyJson == 0 || qtyJson == null) {
+//
+//			print ("No quantity");
+//			qtyJson = 1;
+//
+//		}
+
+		// if no name:
+//		if (newNameJson == "") {
+//			// the new object will just have the name as the original object
+//			newNameJson = subject.name;
+//		}
+
+		// if a destination is specified
+		if (action == "") {
+
+			print ("No action - just the subject");
+			//StartCoroutine(CreateObject (subject, spawnPoint, newNameJson, qtyJson));
+
+		} else {
+
+			//GameObject destination = GameObject.Find (action);
+			print ("Action: " + action);
+
+			//StartCoroutine(CreateObject (subject, destination, newNameJson, qtyJson));
+
+		}
+
+	}
+
+	// Coroutine: Instantiate an object at point
+	IEnumerator CreateObject(GameObject subject, GameObject destination, string new_name, int qty)
+	{
+		// Debug: print ("Instantiate function called, qty = " + qty);
+		for (int i = 0; i < qty; i++) {
+			Vector3 instantiationPoint = destination.transform.position + new Vector3 (0, yOffset, 0);
+			Instantiate (subject, instantiationPoint, default(Quaternion)).name = new_name;
+		}
+		yield return null;
+
+	}
+
+}
---
+++ Assets/Scripts/_Handle.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public partial class Wit3D : MonoBehaviour {
+
+	void Handle(string textToParse) {
+
+		print (textToParse);
+		var N = JSON.Parse (textToParse);
+		print ("SimpleJSON: " + N.ToString());
+
+		string action = N["entities"]["action"][0]["value"].ToString();
+		action = action.Replace ("\"", "");
+		print ("---------" + action);
+		print ("+++++++++" + N["entities"]["action"][0]["value"].ToString());
+
+		//action = "run";
+		// what function should I call?
+		switch (action)
+		{
+		case "sit":
+			anim.SetInteger ("animation", 3);
+			print ("sit");
+			break;
+		case "run":
+			anim.SetInteger ("animation", 5);
+			print ("run");
+			break;
+		case "lay":
+			anim.SetInteger ("animation", 1);
+			print ("lay");
+			break;
+		case "stop":
+			anim.SetInteger ("animation", 0);
+			print ("idle");
+			break;
+//		case "A":
+//			anim.SetInteger ("animation", 5);
+//			print ("A");
+//			break;
+//		case "O":
+//			anim.SetInteger ("animation", 0);
+//			print ("idle");
+//			break;
+		default:
+			print ("Sorry, didn't understand your intent.");
+			break;
+		}
+
+
+	}
+}
